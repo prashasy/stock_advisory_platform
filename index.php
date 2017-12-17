4a5f532767b48f529f843bd4c0dc3f8c654bc973
Prashasy Ashok <prashasyashok@gmail.com>
2017-12-17 15:05:48 +0530

    UI Updates

--- index.php
+++ index.php
@@ -217,7 +217,7 @@
 					<p id="inputtext"> Your name please</p>
 					<form name="name_form" target="_blank" action='input.php' method="post">
 						<input type="text" name="field_name" value="Name" hidden></input>
-						<input type="text" id='a' required name="name" placeholder="Name">
+						<center><input type="text" id='a' required name="name" placeholder="Name"></center>
 						<br><br><center><input type="button" onclick="showfield('name','age','a')" value="Submit"></center>
 					</form>
 				</div>
@@ -227,7 +227,7 @@
 			<p id="inputtext"> We will also need your Age</p>
 			<form name='age_form' action='input.php' target="_blank" method="post">
 				<input type="text" name="field_name" value="Age" hidden></input>
-				<input type="number" id='b' name="age" required placeholder="Age"></input><br><br>
+				<center><input type="number" id='b' name="age" required placeholder="Age"></input><br><br></center>
 				<center><input type="submit" onclick="showfield('age','phone','b')" value="Submit"></center>
 			</form>
 		</div>
@@ -236,7 +236,7 @@
 			<p id="inputtext"> We would contact you if important</p>
 			<form name='phone_form' action='input.php' target="_blank" method="post">
 				<input type="text" name="field_name" value="Phone" hidden></input>
-				<input type="number" id='c' name="phone" required placeholder="Phone"></input><br>
+				<center><input type="number" id='c' name="phone" required placeholder="Phone"></input></center><br>
 				<br><center><input type="submit" onclick="showfield('phone','aadhar','c')" value="Submit"></center>
 			</form>
 		</div>
@@ -245,7 +245,7 @@
 			<p id="inputtext"> Please enter your 12 digit Aadhar Number</p>
 			<form name='aadhar_form' action='input.php' target="_blank" method="post">
 				<input type="text" name="field_name" value="Aadhar" hidden></input>
-				<input type="number" id='d' name="aadhar" required placeholder="Aadhar Number"></input><br>
+				<center><input type="number" id='d' name="aadhar" required placeholder="Aadhar Number"></input></center><br>
 				<br><center><input type="submit" onclick="showfield('aadhar','invest_amount','d')" value="Submit"></>center
 			</form>
 		</div>
@@ -253,10 +253,10 @@
 			<p id="inputtext"> Please enter investment amount</p>
 			<form name='invest_amount_form' action='input.php' target="_blank" method="post">
 				<br><input type="text" name="field_name" value="Invest_amount" hidden></input>
-				<input type="radio" id='e' name='invest_amount' required value=1>INR 25000</input><br>
+				<center><input type="radio" id='e' name='invest_amount' required value=1>INR 25000</input><br>
 				<input type="radio" id='e' name='invest_amount' required value=2>INR 50000</input><br>
 				<input type="radio" id='e' name='invest_amount' required value=3>INR 75000</input><br>
-				<input type="radio" id='e' name='invest_amount' required value=4>INR 100000</input><br><br>
+				<input type="radio" id='e' name='invest_amount' required value=4>INR 100000</input></center><br><br>
 				<input type="submit" onclick="showfield('invest_amount','risk_level','e')" value="Submit">
 			</form>
 		</div>
@@ -264,9 +264,9 @@
 			<p id="inputtext"> What risk level are you comfortable with?</p>
 			<form name='risk_level_form' action='input.php' target="_blank" method="post">
 			<br><input type="radio" name="risk_level" id='f' required value=1>Risk Averse</input><br>
-				<input type="radio" name="risk_level" id='f' required value=2>Risk Neutral</input><br>
+				<center><input type="radio" name="risk_level" id='f' required value=2>Risk Neutral</input><br>
 				<input type="radio" name="risk_level" id='f' required value=3>Risk Seeker</input><br><br>
-				<input type="text" name="field_name" id='f' required value="Risk_level" hidden></input>
+				<input type="text" name="field_name" id='f' required value="Risk_level" hidden></center></input>
 				<input type="submit" onclick="showfield('risk_level','invest_horizon','f')" value="Submit">
 			</form>
 		</div>
@@ -275,11 +275,11 @@
 			<p id="inputtext"> What would be your Investment Horizon?</p>
 			<form name='invest_horizon_form' action='input.php' target="_blank" method="post">
 			<br><input type="text" name="field_name" value="Invest_horizon" hidden></input>
-				<input type="radio" name="invest_horizon" id='g' required value=1>1 year</input><br>
+				<center><input type="radio" name="invest_horizon" id='g' required value=1>1 year</input><br>
 				<input type="radio" name="invest_horizon" id='g' required value=1>2 year</input><br>
 				<input type="radio" name="invest_horizon" id='g' required value=1>3 year</input><br>
 				<input type="radio" name="invest_horizon" id='g' required value=1>4 year</input><br>
-				<input type="radio" name="invest_horizon" id='g' required value=1>5 year</input><br><br>
+				<input type="radio" name="invest_horizon" id='g' required value=1>5 year</input></center><br><br>
 
 				<input type="submit" onclick="showfield('invest_horizon','portf_type','g')" value="Submit">
 			</form>
@@ -288,10 +288,10 @@
 		<div class="field" id="portf_type" style="text-align:left" align="center">
 			<p id="inputtext"> Please select your portfolio type</p>
 			<form name='portf_type_form' action='input.php' target="_blank" method="post">
-				<input type="text" name="field_name" value="Portf_type" hidden></input>
+				<center><input type="text" name="field_name" value="Portf_type" hidden></input>
 				<input type="radio" name="portf_type" id='h' required value=1>Point K2 Flagship portfolio</input><br>
 				<input type="radio" name="portf_type" id='h' required value=2>Sector portfolio</input><br>
-				<input type="radio" name="portf_type" id='h' required value=3>Formula Investing Portfolio</input><br><br>
+				<input type="radio" name="portf_type" id='h' required value=3>Formula Investing Portfolio</input></center><br><br>
 				<input type="submit" onclick="showfield('portf_type','portf_disp','h')" value="Submit">
 			</form>
 		</div>
